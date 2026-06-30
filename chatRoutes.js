e1d0beec0139cb4264f4d14185588297d326928c
yujimiakii <20241ac0080061@estudantes.ifpr.edu.br>
2026-06-30 14:09:10 -0300

    Delete chatRoutes.js

--- chatRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const chatController = require('../controllers/chatController');
-
-router.post('/', chatController.enviarPergunta);
-router.delete('/limpar', chatController.limparHistorico);
-
-module.exports = router;
